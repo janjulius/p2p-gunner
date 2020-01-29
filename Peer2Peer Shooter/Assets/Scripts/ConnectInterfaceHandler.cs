ea0cdf5f6dc2cf86fae0ae495c051a0156f38b35
Jan Julius <jdelang@hotmail.com>
2020-01-29 17:37:24 +0100

    basic connection22

---
+++ Peer2Peer Shooter/Assets/Scripts/ConnectInterfaceHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ConnectInterfaceHandler : MonoBehaviour
+    {
+        public TextMeshProUGUI ip;
+        public TextMeshProUGUI port;
+        public TextMeshProUGUI maxPlayers;
+        public TextMeshProUGUI pwd;
+
+        public NetworkManager NetworkManager;
+
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        public void Connect()
+        {
+            int p = 9050;
+            if (!Int32.TryParse(port.text, out p))
+                p = 9050;
+            NetworkManager.InitGame(ip.text, p, pwd.text, -1, false);
+        }
+
+        public void Host()
+        {
+            int p = 9050;
+            if (!Int32.TryParse(port.text, out p))
+                p = 9050;
+            int maxp = 10;
+            if (!Int32.TryParse(maxPlayers.text, out maxp))
+                maxp = 10;
+            NetworkManager.InitGame("", p, pwd.text, maxp, true);
+        }
+
+    }
+}
---
+++ Peer2Peer Shooter/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,130 @@
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager Instance;
+    Thread networkThread;
+    Thread mainThread;
+
+    string myName;
+    Color myColor;
+
+    private bool stopConnection = false;
+    public EventBasedNetListener networkListener;
+    public NetManager client;
+
+    public bool isHost;
+
+    private string host = "localhost";
+    private int port;
+    private string password;
+    private int maxPlayers;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        UnityThread.initUnityThread();
+    }
+
+    public void InitGame(string hostaddress, int port, string pwd, int maxPlayers, bool isHost)
+    {
+        stopConnection = false;
+        this.isHost = isHost;
+        this.maxPlayers = maxPlayers;
+        this.password = pwd;
+        this.host = hostaddress == "" ? "localhost" : hostaddress;
+        this.port = port;
+        
+        myName = "test " + Random.value;
+        myColor = new Color(Random.value, Random.value, Random.value);
+        print($"Initgame: Name:{myName} color:{myColor.ToString()}");
+        mainThread = Thread.CurrentThread;
+        networkThread = new Thread(UpdateNetwork);
+        networkThread.Start();
+    }
+
+    public void UpdateNetwork()
+    {
+        networkListener = new EventBasedNetListener();
+
+        networkListener.NetworkReceiveEvent += ReceivePackage;
+        networkListener.PeerDisconnectedEvent += (peer, info) =>
+        {
+            Debug.Log(info.Reason.ToString());
+            stopConnection = true;
+        };
+
+        client = new NetManager(networkListener);
+        if (isHost)
+            client.Start(port);
+        else
+            client.Start();
+
+        if (isHost)
+        {
+            print($"Starting as host {host}:{port} with pwd {password}");
+            networkListener.ConnectionRequestEvent += OnConnectionRequestEvent;
+            networkListener.PeerConnectedEvent += OnPeerConnectedEvent;
+        }
+        else
+        {
+            print($"Trying to connect to: {host}:{port} with pwd {password}");
+            client.Connect(host, port, password);
+        }
+
+        NetDataWriter writer = new NetDataWriter();
+        while (!stopConnection)
+        {
+            client.PollEvents();
+            Thread.Sleep(16);
+            print("polling :" + client.ConnectedPeerList.Count);
+        }
+        client.Stop();
+        print("End of updatenetwork");
+    }
+
+    void ReceivePackage(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+    {
+        NetDataWriter writer = new NetDataWriter();
+        ushort msgid = reader.GetUShort();
+        switch (msgid)
+        {
+            case 1:
+
+                break;
+        }
+    }
+
+    void InitPlayer(string name, Color color)
+    {
+
+    }
+
+    public void OnConnectionRequestEvent(ConnectionRequest request)
+    {
+        print("Received connection request");
+        if (client.PeersCount < maxPlayers)
+            request.AcceptIfKey(password);
+        else
+            request.Reject();
+    }
+
+    public void OnPeerConnectedEvent(NetPeer peer)
+    {
+        print("connection from: " + peer.EndPoint);
+        NetDataWriter writer = new NetDataWriter();
+        writer.Put("test");
+        peer.Send(writer, DeliveryMethod.ReliableOrdered);
+    }
+
+    private void OnApplicationQuit()
+    {
+        client.Stop();
+    }
+
+}
